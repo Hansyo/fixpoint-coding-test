400cce3b4cd376d09d96c48242b0e0a91439d266
Hansyo <hansyo.souti.1999@gmail.com>
2022-11-27 23:33:05 +0900

    add: exam3 test case

--- tests/test_001_server.py
+++ tests/test_001_server.py
@@ -51,3 +51,68 @@ def test_get_downtimes_03():
         (DT.datetime(2020, 10, 19, 13, 31, 50), DT.datetime(2020, 10, 19, 13, 31, 55)),
         (DT.datetime(2020, 10, 19, 13, 31, 56), None),
     ]
+
+
+def test_get_downtimes_04():
+    file_path = "test_case/003_03.csv"
+    servers = Server.load_data(file_path=file_path)
+    assert servers["10.20.30.1"].get_downtimes(5) == [
+        (DT.datetime(2020, 10, 19, 13, 31, 26), DT.datetime(2020, 10, 19, 13, 31, 31)),
+        (DT.datetime(2020, 10, 19, 13, 32, 6), DT.datetime(2020, 10, 19, 13, 32, 11)),
+    ]
+
+
+def test_get_overload_01():
+    file_path = "test_case/003_00.csv"
+    servers = Server.load_data(file_path=file_path)
+    assert servers["10.20.30.1"].get_overload_times() == [
+        (DT.datetime(2020, 10, 19, 13, 31, 41), None),
+    ]
+    assert servers["10.20.30.2"].get_overload_times() == []
+
+
+def test_get_overload_02():
+    file_path = "test_case/003_01.csv"
+    servers = Server.load_data(file_path=file_path)
+    assert servers["10.20.30.1"].get_overload_times(continuous=3) == [
+        (DT.datetime(2020, 10, 19, 13, 31, 43), DT.datetime(2020, 10, 19, 13, 31, 44)),
+        (DT.datetime(2020, 10, 19, 13, 31, 47), DT.datetime(2020, 10, 19, 13, 31, 49)),
+        (DT.datetime(2020, 10, 19, 13, 31, 52), DT.datetime(2020, 10, 19, 13, 31, 55)),
+        (DT.datetime(2020, 10, 19, 13, 31, 58), None),
+    ]
+
+
+def test_get_overload_03():
+    file_path = "test_case/003_01.csv"
+    servers = Server.load_data(file_path=file_path)
+    assert servers["10.20.30.1"].get_overload_times(continuous=5) == [
+        (DT.datetime(2020, 10, 19, 13, 31, 54), DT.datetime(2020, 10, 19, 13, 31, 55)),
+        (DT.datetime(2020, 10, 19, 13, 32, 00), None),
+    ]
+
+
+def test_get_overload_04():
+    file_path = "test_case/003_01.csv"
+    servers = Server.load_data(file_path=file_path)
+    assert servers["10.20.30.1"].get_overload_times(time_threshold=60) == [
+        (DT.datetime(2020, 10, 19, 13, 31, 38), None),
+    ]
+
+
+def test_get_overload_05():
+    file_path = "test_case/003_01.csv"
+    servers = Server.load_data(file_path=file_path)
+    assert servers["10.20.30.1"].get_overload_times(continuous=5, time_threshold=80) == [
+        (DT.datetime(2020, 10, 19, 13, 31, 42), DT.datetime(2020, 10, 19, 13, 31, 44)),
+        (DT.datetime(2020, 10, 19, 13, 31, 45), None),
+    ]
+
+
+def test_get_overload_06():
+    file_path = "test_case/003_03.csv"
+    servers = Server.load_data(file_path=file_path)
+    assert servers["10.20.30.1"].get_overload_times(continuous=5, time_threshold=60) == [
+        (DT.datetime(2020, 10, 19, 13, 31, 35), DT.datetime(2020, 10, 19, 13, 31, 59)),
+        (DT.datetime(2020, 10, 19, 13, 32, 3), DT.datetime(2020, 10, 19, 13, 32, 6)),
+        (DT.datetime(2020, 10, 19, 13, 32, 11), None),
+    ]
---
+++ tests/test_exam/test_exam3.py
@@ -0,0 +1,146 @@
+from fixpoint_coding_test import Server
+
+
+def test_exam3_00_01(capfd):
+    file_path = "test_case/003_00.csv"
+    servers = Server.load_data(file_path=file_path)
+    Server.print_server_overload(servers=servers)
+    out, _ = capfd.readouterr()
+    assert (
+        out
+        == """10.20.30.1 has overload
+    2020-10-19 13:31:41 ~ 
+10.20.30.2 has no overload
+"""
+    )
+
+
+def test_exam3_01_01(capfd):
+    file_path = "test_case/003_01.csv"
+    servers = Server.load_data(file_path=file_path)
+    Server.print_server_overload(servers=servers, continuous=1, time_threshold=100)
+    out, _ = capfd.readouterr()
+    assert (
+        out
+        == """10.20.30.1 has overload
+    2020-10-19 13:31:36 ~ 2020-10-19 13:31:37
+    2020-10-19 13:31:38 ~ 2020-10-19 13:31:40
+    2020-10-19 13:31:41 ~ 2020-10-19 13:31:44
+    2020-10-19 13:31:45 ~ 2020-10-19 13:31:49
+    2020-10-19 13:31:50 ~ 2020-10-19 13:31:55
+    2020-10-19 13:31:56 ~ 
+"""
+    )
+
+
+def test_exam3_01_02(capfd):
+    file_path = "test_case/003_01.csv"
+    servers = Server.load_data(file_path=file_path)
+    Server.print_server_overload(servers=servers, continuous=3, time_threshold=100)
+    out, _ = capfd.readouterr()
+    assert (
+        out
+        == """10.20.30.1 has overload
+    2020-10-19 13:31:43 ~ 2020-10-19 13:31:44
+    2020-10-19 13:31:47 ~ 2020-10-19 13:31:49
+    2020-10-19 13:31:52 ~ 2020-10-19 13:31:55
+    2020-10-19 13:31:58 ~ 
+"""
+    )
+
+
+def test_exam3_01_03(capfd):
+    file_path = "test_case/003_01.csv"
+    servers = Server.load_data(file_path=file_path)
+    Server.print_server_overload(servers=servers, continuous=6, time_threshold=100)
+    out, _ = capfd.readouterr()
+    assert (
+        out
+        == """10.20.30.1 has overload
+    2020-10-19 13:32:01 ~ 
+"""
+    )
+
+
+def test_exam3_01_04(capfd):
+    file_path = "test_case/003_01.csv"
+    servers = Server.load_data(file_path=file_path)
+    Server.print_server_overload(servers=servers, continuous=7, time_threshold=100)
+    out, _ = capfd.readouterr()
+    assert (
+        out
+        == """10.20.30.1 has no overload
+"""
+    )
+
+
+def test_exam3_02_01(capfd):
+    file_path = "test_case/003_02.csv"
+    servers = Server.load_data(file_path=file_path)
+    Server.print_server_overload(servers=servers, continuous=3, time_threshold=1)
+    out, _ = capfd.readouterr()
+    assert (
+        out
+        == """10.20.30.1 has overload
+    2020-10-19 13:31:24 ~ 
+"""
+    )
+
+
+def test_exam3_02_02(capfd):
+    file_path = "test_case/003_02.csv"
+    servers = Server.load_data(file_path=file_path)
+    Server.print_server_overload(servers=servers, continuous=3, time_threshold=5)
+    out, _ = capfd.readouterr()
+    assert (
+        out
+        == """10.20.30.1 has overload
+    2020-10-19 13:31:28 ~ 
+"""
+    )
+
+
+def test_exam3_02_03(capfd):
+    file_path = "test_case/003_02.csv"
+    servers = Server.load_data(file_path=file_path)
+    Server.print_server_overload(servers=servers, continuous=3, time_threshold=30)
+    out, _ = capfd.readouterr()
+    assert (
+        out
+        == """10.20.30.1 has overload
+    2020-10-19 13:31:53 ~ 
+"""
+    )
+
+
+def test_exam3_02_04(capfd):
+    file_path = "test_case/003_02.csv"
+    servers = Server.load_data(file_path=file_path)
+    Server.print_server_overload(servers=servers, continuous=5, time_threshold=30)
+    out, _ = capfd.readouterr()
+    assert (
+        out
+        == """10.20.30.1 has overload
+    2020-10-19 13:31:54 ~ 
+"""
+    )
+
+
+def test_exam3_03_01(capfd):
+    file_path = "test_case/003_03.csv"
+    servers = Server.load_data(file_path=file_path)
+    Server.print_server_error(servers=servers, continuous=3, time_threshold=100)
+    out, _ = capfd.readouterr()
+    assert (
+        out
+        == """10.20.30.1 has error
+    downtime 2020-10-19 13:31:26 ~ 2020-10-19 13:31:31
+    overload 2020-10-19 13:31:36 ~ 2020-10-19 13:31:39
+    downtime 2020-10-19 13:31:39 ~ 2020-10-19 13:31:43
+    overload 2020-10-19 13:31:43 ~ 2020-10-19 13:31:53
+    downtime 2020-10-19 13:31:55 ~ 2020-10-19 13:31:59
+    overload 2020-10-19 13:32:03 ~ 2020-10-19 13:32:06
+    downtime 2020-10-19 13:32:06 ~ 2020-10-19 13:32:11
+    overload 2020-10-19 13:32:11 ~ 
+"""
+    )
